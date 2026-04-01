ad3557d03adea3fe7e16568bdd3912a7dc27e757
Morph_Mojang <morphmojang@gmail.com>
2026-04-01 14:45:45 +0100

    Add files via upload

---
+++ builder+.bat
@@ -0,0 +1,10 @@
+@echo off
+title Builder+ (Modrinth Mod)
+
+:list
+cls
+echo ~~~~~ Welcome %username% ~~~~~
+echo.
+echo 1) Open Builder+ On Modrinth
+set /p open=Enter Number:   
+if "%open%"=="1" start https://modrinth.com/project/cUq9U3DK
